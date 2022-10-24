ad806ca2e3467682300d35323432151d8da60b44
nalyx1 <joaorodriguez0106@gmail.com>
2022-10-24 16:25:25 -0300

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API,
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const getRepositories = async (userId: string, query: string) => {
